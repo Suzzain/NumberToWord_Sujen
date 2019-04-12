f0a226b69c9a377b33d2d20bb46c565d8f6664a3
Suzzain <gurungsujen@gmail.com>
2019-04-12 20:50:27 +0545

    Initial commit

---
+++ app/src/main/java/com/e/numbertoword_sujen/MainActivity.java
@@ -0,0 +1,34 @@
+package com.e.numbertoword_sujen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+EditText etNumber;
+TextView tvWords;
+Button btnConvert;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNumber = findViewById(R.id.etNumber);
+        tvWords = findViewById(R.id.tvWords);
+        btnConvert = findViewById(R.id.btnConvert);
+
+        btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num;
+                num= Integer.parseInt(etNumber.getText().toString());
+
+                NumtoWord numtoword = new NumtoWord(num);
+                tvWords.setText(numtoword.Words());
+            }
+        });
+    }
+}
